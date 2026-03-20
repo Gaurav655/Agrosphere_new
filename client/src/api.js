c4b52041c6a708055425a4aa4673197090aa2e9b
Gaurav Kesharwani <gkesharwani655@gmail.com>
2026-03-20 16:05:34 +0530

    Update API URL for production

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URI = 'http://localhost:5000/api';
+const API_URI = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
 
 const api = axios.create({
   baseURL: API_URI,
